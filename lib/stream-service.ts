9a423133fe4980daeaa162c48677179a7f370f5e
davit <datokikvadze8@gmail.com>
2024-07-02 18:05:59 +0400

    feat: chat Setting page and functionaliti changeing database states of ischatenable,ischatdelayed and isChatFollowersOnlay

---
+++ lib/stream-service.ts
@@ -0,0 +1,8 @@
+import db from "./db";
+
+export const getStreamByUserid = async (userId: string) => {
+    const stream = await db.stream.findUnique({
+        where: { userId },
+    });
+    return stream;
+};
